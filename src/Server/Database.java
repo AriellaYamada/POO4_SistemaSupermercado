be96f9217289b5e39f86e7582b629f146e8bb976
Carlos Schneider <carlosaschjr@gmail.com>
2015-06-17 15:14:01 -0300

    Continua o Servidor

--- src/Server/Database.java
+++ src/Server/Database.java
@@ -13,6 +13,27 @@ public class Database {
 	private Database (){}
 
 	public static String process (String line){
+		String[] cmd = line.split(",");
+		switch (cmd[0]){
+			case "newuser":
+				break;
+			case "login":
+				break;
+			case "search":
+				break;
+			case "startsell":
+				break;
+			case "reserve":
+				break;
+			case "dereserve":
+				break;
+			case "cancelsell":
+				break;
+			case "confirmsell":
+				break;
+			default:
+				break;
+		}
 		return line;
 	}
 }
