8b964919b657cf076db097dd9dc870141e6ddd87
Nathália <nath.felippe23@gmail.com>
2015-06-20 16:34:11 -0300

    usersDatabase

---
+++ src/Database/UsersDatabase.java
@@ -0,0 +1,48 @@
+package Database;
+
+import java.io.IOException;
+
+import static java.lang.System.out;
+
+public class UsersDatabase extends Database {
+
+
+	public void WriteFile() {
+		OpenWriter(false);
+		final String SEPARATOR = ",";
+		final String ENDLINE = "\n";
+		final String HEADER = "name,adress, telephone, email, id, password";
+
+		try {
+			fw.append(HEADER);
+			fw.append(ENDLINE);
+			fw.flush();
+
+
+			fw.flush();
+
+		} catch (IOException e){
+			out.println("Erro na escrita do arquivo.");
+			e.printStackTrace();
+		}
+
+	}
+
+	public void ReadFile() {
+		OpenReader();
+
+		String line;
+		String splitSign = ",";
+
+		try {
+			br.readLine();
+			while ((line = br.readLine()) != null) {
+				String[] splited = line.split(splitSign);
+				Users.getInstance().Register(splited[0], splited[1], splited[2], splited[3], splited[4], splited[5]);
+			}
+		} catch (IOException e) {
+			out.println("Erro na leitura do arquivo.");
+			e.printStackTrace();
+		}
+	}
+}
