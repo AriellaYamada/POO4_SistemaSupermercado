dcc6d2367c929044fd1f3bfcb907f2a4a7d75115
AriellaYamada <ariella.y.b@hotmail.com>
2015-06-20 17:20:25 -0300

    montando sales

--- src/Database/Sales.java
+++ src/Database/Sales.java
@@ -1,4 +1,6 @@
 package Database;
 
 public class Sales {
+
+
 }
--- src/Server/Sale.java
+++ src/Server/Sale.java
@@ -1,4 +1,14 @@
 package Server;
 
 public class Sale {
+
+	private String clientId;
+	private String product;
+	private int quantity;
+
+	public Sale (String clientId, String product, int quantity) {
+		this.clientId = clientId;
+		this.product = product;
+		this.quantity = quantity;
+	}
 }
