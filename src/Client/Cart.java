ecc88ccd4972f6bf2bb22fe85263d6444daf7e4f
AriellaYamada <ariella.y.b@hotmail.com>
2015-06-26 17:00:49 -0300

    Modificando o carrinho

--- src/Client/Cart.java
+++ src/Client/Cart.java
@@ -3,13 +3,14 @@ package Client;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import Structure.Product;
 
 public class Cart {
 
 	private static Cart cart;
 
-	private List<OnCart> products;
-	private Stream<OnCart> filtered;
+	private List<Product> products;
+	private Stream<Product> filtered;
 
 	//Singleton
 	public static Cart getInstance() {
@@ -18,25 +19,22 @@ public class Cart {
 		return cart;
 	}
 
-	public boolean CheckCart(String product) {
+	public boolean CheckCart(Product product) {
 		filtered = products.stream();
-		filtered = filtered.filter(p -> p.getName().equals(product));
+		filtered = filtered.filter(p -> p.equals(product));
 		return (filtered.count() == 0);
 	}
 
-	public void AddToCart(String name, float price) {
-		if(CheckCart(name)) {
-			OnCart NewItem = new OnCart(name, price);
-			products.add(NewItem);
+	public void AddToCart(Product selProduct) {
+		if(CheckCart(selProduct)) {
+			products.add(selProduct);
 		} else {
-			AddOne(name);
+
 		}
 	}
 
 	public void AddOne(String product) {
-		filtered = products.stream();
-		filtered = filtered.filter(p -> p.getName().equals(product));
-		filtered.collect(Collectors.toList()).get(0).AddOne();
+
 	}
 
 	public void RefreshQtd(String name, int newQtd) {
