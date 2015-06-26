5198d603ad9f0e1c6ee704c86eb3a8bd308b9658
Carlos Schneider <carlosaschjr@gmail.com>
2015-06-26 17:00:48 -0300

    implementa novas funções no carrinho

--- src/Interface/Client/Controller/cartListController.java
+++ src/Interface/Client/Controller/cartListController.java
@@ -1,14 +1,23 @@
 package Interface.Client.Controller;
 
+import Interface.MainInterface;
 import Structure.Product;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
+
 public class cartListController {
 
+	@FXML public Label l_total_value;
+	@FXML public VBox alertDialog;
+	@FXML public VBox clearDialog;
+	@FXML public VBox confirmDialog;
 	@FXML private TableView<Product> tv_table;
 	@FXML private TableColumn<Product, String> c_name;
 	@FXML private TableColumn<Product, Float> c_price;
@@ -27,5 +36,30 @@ public class cartListController {
 	@FXML
 	void dismiss() {
 		alert.setVisible(false);
+	}
+
+	public void backToMenu() {
+		try {
+			MainInterface.changeScene("Client/Model/menu.fxml");
+		} catch (IOException e) {
+			System.err.println("Erro ao exibir tela");
+		}
+	}
+
+	@FXML
+	public void showClearDialog() {
+		clearDialog.setVisible(true);
+	}
+
+	@FXML
+	public void showConfirmDialog() {
+	}
+
+	@FXML
+	public void confirmClear() {
+	}
+
+	@FXML
+	public void confirmEndSale() {
 	}
 }
